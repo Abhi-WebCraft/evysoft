2d50fbe87c7b0e4992c5f8bd868df33d3c96911d
Abhi-Dev-code <abhishek.webcraft@gmail.com>
2025-04-09 15:57:37 +0530

    update

--- src/components/blog/PostTitle.jsx
+++
@@ -1,8 +0,0 @@
-export default function PostTitle({ children }) {
-  return (
-    <h1
-className="text-xl md:text-3xl lg:text-4xl font-bold hover:text-[#28aa4a] tracking-tighter leading-tight md:leading-none mb-8 text-center md:text-left transition duration-300 ease-in-out"
-      dangerouslySetInnerHTML={{ __html: children }}
-    />
-  );
-}
