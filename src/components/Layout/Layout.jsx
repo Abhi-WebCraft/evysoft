83add73f6d8dc476277472ec006cb5dc67d486cb
Abhi-WebCraft <abhishek.webcraft@gmail.com>
2025-05-17 19:36:58 +0530

    dddd

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -14,6 +14,14 @@ export default function Layout({children}) {
     <>
         <Header />
         {children}
+          <WhatsAppChat
+        phoneNumber="7341173250"
+        accountName="Welcome to Evysoft –"
+        statusMessage=" How Can We Power Your Digital Vision Today?"
+        chatMessage="Hi there! How can we help you today?"
+        avatar="/images/icons/evy.png"
+        serviceOptions="Custom Software Development, Web Application Development,Mobile App Development, Artificial Intelligence,Process Automation,IT Consulting & Strategy"
+      />
         <Footer />
        
     </>
